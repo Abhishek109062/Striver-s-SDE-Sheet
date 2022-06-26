3eae84dc59297e91329034a76b032f6d2c7404a7
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-06-26 08:51:52 +0530

    Create main.java

---
+++ Set Matrix Zero/main.java
@@ -0,0 +1,30 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static void setZeros(int matrix[][]) {
+        int n=matrix.length;
+    int m=matrix[0].length;
+    int x=1;
+    
+    for(int i=0;i<n;i++){
+        if(matrix[i][0]==0) x=0;
+        for(int j=1;j<m;j++){
+            if(matrix[i][j]==0){
+                matrix[i][0]=0;
+                matrix[0][j]=0;
+            }
+        }
+    }
+    for(int i=n-1;i>=0;i--){
+        for(int j=m-1;j>=1;j--){
+            if(matrix[i][0]==0 || matrix[0][j]==0){
+                matrix[i][j]=0;
+            }
+        }
+        if(x==0){
+            matrix[i][0]=0;
+        }
+    }
+    }
+
+}
